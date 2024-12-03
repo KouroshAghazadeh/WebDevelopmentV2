1c94e388c280704d19c4c101986ccd175020eebc
KouroshAghazadeh <kouroshaghazadeh1370@gmail.com>
2024-12-03 12:37:25 +0330

    030914

--- database.py
+++ database.py
@@ -1,9 +1,9 @@
-from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine, text
 
 
 db_connection = "mysql://avnadmin:AVNS_oXmc4rbaAybAi7fOIBt@mysql-3d3eacf2-kouroshcareer.b.aivencloud.com:21821/defaultdb?charset=utf8mb4"
 
-engine = SQLAlchemy.create_engine(
+engine = create_engine(
     db_connection,
     connect_args={
         'ssl': {
@@ -14,7 +14,7 @@ engine = SQLAlchemy.create_engine(
 
 def load_jobs_from_db():
     with engine.connect() as conn:
-        result = conn.execute(SQLAlchemy.text('select * from jobs')).fetchall()
+        result = conn.execute(text('select * from jobs')).fetchall()
         jobs = []
         for i in range(len(result)):
             row_dict = result[i]._mapping
